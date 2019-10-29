94de92ed008ed80b427f52c51c440459ea2cab67
David Fernandez <morenza@gmail.com>
2019-10-29 22:53:43 +0000

    Added auth and get pods commands

---
+++ README.md
@@ -0,0 +1,9 @@
+# MyIAC
+
+Infrastructure as code. GCP for now.
+
+## Setup
+
+* Create a GCP service account from here: https://cloud.google.com/iam/docs/creating-managing-service-accounts#iam-service-accounts-create-console
+* Download the json file and store in your user dir.
+* MyIAC will take control from there.
--- app/main.go
+++ app/main.go
@@ -5,18 +5,35 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
+	"os/user"
 	"strings"
 )
 
 //https://blog.kowalczyk.info/article/wOYk/advanced-command-execution-in-go-with-osexec.html
+//https://golang.org/doc/code.html
 func main() {
 	fmt.Printf("MyIaC - Infrastructure as Code\n")
-	//command("ls", "-lah")
 	setupEnvironment()
-	//https://golang.org/doc/code.html
+	setupKubernetes()
+	getPods()
 }
 
 func setupEnvironment() {
+	// create a service account and download it: 
+	keyLocation := getHomeDir() + "/account.json"
+	baseArgs := "auth activate-service-account --key-file %s"
+	baseArgsTmpl := fmt.Sprintf(baseArgs, keyLocation)
+	var argsArray []string = strings.Fields(baseArgsTmpl)
+	command("gcloud", argsArray)
+}
+
+func getPods() {
+	baseArgs := "get pods"
+	var argsArray []string = strings.Fields(baseArgs)
+	command("kubectl", argsArray)
+}
+
+func setupKubernetes() {
 	project := "moneycol"
 	zone := "europe-west1-b"
 	clusterName := "moneycol-main"
@@ -31,10 +48,18 @@ func command(command string, arguments []string) {
 	cmd := exec.Command(command, arguments...)
 	//cmd.Dir = "/Users/david"
 	fmt.Printf("Executing [ %s ]\n", string(strings.Join(cmd.Args, " ")))
-	out, err := cmd.CombinedOutput()
+	out, err := cmd.CombinedOutput() //TODO: get stderr and stdout in separate strings
 	if err != nil {
 		fmt.Printf("Output: \n%s\n", string(out))
 		log.Fatalf("cmd.Run() failed with %s\n", err)
 	}
 	fmt.Printf("Output: \n%s\n", string(out))
+}
+
+func getHomeDir() string {
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return usr.HomeDir
 }
