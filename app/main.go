648c7dfcd32a5b62e2e61558ba7f1e538c734e5e
David Fernandez <morenza@gmail.com>
2019-11-04 16:13:28 +0000

    More commands and automation: kubectl, labelling, docker...

--- app/main.go
+++ app/main.go
@@ -13,13 +13,18 @@ import (
 //https://golang.org/doc/code.html
 func main() {
 	fmt.Printf("MyIaC - Infrastructure as Code\n")
+	runtime := RuntimeProperties{}
 	setupEnvironment()
-	setupKubernetes()
-	getPods()
+	configureDocker()
+	tagDockerImage(&runtime)
+	//setupKubernetes()
+	//getPods()
+	//labelElasticsearchNodes()
+	//labelDockerImage()
 }
 
 func setupEnvironment() {
-	// create a service account and download it: 
+	// create a service account and download it:
 	keyLocation := getHomeDir() + "/account.json"
 	baseArgs := "auth activate-service-account --key-file %s"
 	baseArgsTmpl := fmt.Sprintf(baseArgs, keyLocation)
@@ -44,7 +49,44 @@ func setupKubernetes() {
 	command("gcloud", argsArray)
 }
 
-func command(command string, arguments []string) {
+func labelElasticsearchNodes() {
+	nodeName := "gke-moneycol-main-elasticsearch-pool-b8711571-k359"
+	label := "type=elasticsearch"
+	labelCmdTpl := "label nodes %s %s"
+	argsStr := fmt.Sprintf(labelCmdTpl, nodeName, label)
+	argsArray := strings.Fields(argsStr)
+	command("kubectl", argsArray)
+}
+
+func configureDocker() {
+	action := "auth configure-docker"
+	cmdTpl := "%s"
+	argsStr := fmt.Sprintf(cmdTpl, action)
+	argsArray := strings.Fields(argsStr)
+	command("gcloud", argsArray)
+}
+
+func tagDockerImage(runtime *RuntimeProperties) {
+	imageToTag := "c58e8ce1a62e"
+	projectId := "moneycol"
+	projectRepository := "gcr.io"
+	containerName := "moneycol-server"
+	tag := "0.1.0-alpha1"
+	containerFullName := fmt.Sprintf("%s/%s/%s:%s", projectRepository, projectId, containerName, tag)
+	fmt.Printf("The image tag to push is: %s\n", containerFullName)
+	dockerTagCmdPart := "tag"
+	argsStr := fmt.Sprintf("%s %s %s\n", dockerTagCmdPart, imageToTag, containerFullName)
+	argsArray := strings.Fields(argsStr)
+	fmt.Printf("Array of args %s\n", argsArray)
+	err := command("docker", argsArray)
+	if err != nil {
+		log.Fatalf("Command '%s' failed with error %s\n", "docker "+argsStr, err)
+	}
+	runtime.SetDockerImage(containerFullName)
+	fmt.Printf("Docker image has been tagged with: %s\n", runtime.GetDockerImage())
+}
+
+func command(command string, arguments []string) error {
 	cmd := exec.Command(command, arguments...)
 	//cmd.Dir = "/Users/david"
 	fmt.Printf("Executing [ %s ]\n", string(strings.Join(cmd.Args, " ")))
@@ -52,8 +94,10 @@ func command(command string, arguments []string) {
 	if err != nil {
 		fmt.Printf("Output: \n%s\n", string(out))
 		log.Fatalf("cmd.Run() failed with %s\n", err)
+		return err
 	}
 	fmt.Printf("Output: \n%s\n", string(out))
+	return nil
 }
 
 func getHomeDir() string {
@@ -62,4 +106,18 @@ func getHomeDir() string {
 		log.Fatal(err)
 	}
 	return usr.HomeDir
+}
+
+// ------------------- separate this -----
+
+type RuntimeProperties struct {
+	DockerImage string
+}
+
+func (rp *RuntimeProperties) SetDockerImage(dockerImage string) {
+	rp.DockerImage = dockerImage
+}
+
+func (rp RuntimeProperties) GetDockerImage() string {
+	return rp.DockerImage
 }
