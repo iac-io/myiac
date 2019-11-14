ca7b8069ab62d82a7b94ccbe12c6b45a234e5299
David Fernandez <morenza@gmail.com>
2019-11-14 00:34:11 +0000

    Started docker tag task

--- app/docker/docker.go
+++ app/docker/docker.go
@@ -20,11 +20,10 @@ func TagDockerImage(dockerImageDef *DockerImageDefinition) {
 	containerFullName := fmt.Sprintf("%s/%s/%s:%s", dockerImageDef.projectRepository, dockerImageDef.projectId, dockerImageDef.containerName, tag)
 	fmt.Printf("The image tag to push is: %s\n", containerFullName)
 	dockerTagCmdPart := "tag"
-	dockerCmd := []string{dockerTagCmdPart, dockerImageDef.imageIdToTag, containerFullName}
-	argsArray := util.StringTemplateToArgsArray("%s %s %s", dockerCmd)
+	argsArray := util.StringTemplateToArgsArray("%s %s %s", dockerTagCmdPart, dockerImageDef.imageIdToTag, containerFullName)
 	cmd := commandline.New("docker", argsArray)
 	cmd.Run()
-	
+	//TODO: continue
 	//runtime.SetDockerImage(containerFullName)
 	fmt.Printf("Docker image has been tagged with: %s\n", containerFullName)
 }
