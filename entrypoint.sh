0bcd1d2b9cffc7cb2ed411a753e577b0b40f93e0
David Fernandez <morenza@gmail.com>
2019-12-12 09:10:53 +0000

    Remove nsenter

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,30 +8,30 @@ set -o nounset
 set -o pipefail
 
 CHECK_INTERVAL_SECONDS="30"
-EXEC=(nsenter -t 1 -m -u -i -n -p --)
+#EXEC=(nsenter -t 1 -m -u -i -n -p --)
 CHECKPOINT_PATH="/tmp/foo"
 
 echo "====== Startup script Daemonset ====="
 
 do_startup_script() {
   local err=0;
-
-  "${EXEC[@]}" bash -c "${STARTUP_SCRIPT}" && err=0 || err=$?
+  bash -c  "${STARTUP_SCRIPT}" && err=0 || err=$?
+  #"${EXEC[@]}" bash -c "${STARTUP_SCRIPT}" && err=0 || err=$?
   if [[ ${err} != 0 ]]; then
     echo "!!! startup-script failed! exit code '${err}'" 1>&2
     return 1
   fi
 
-  "${EXEC[@]}" touch "${CHECKPOINT_PATH}"
+  touch "${CHECKPOINT_PATH}"
   echo "!!! startup-script succeeded!" 1>&2
   return 0
 }
 
 while :; do
-  "${EXEC[@]}" stat "${CHECKPOINT_PATH}" > /dev/null 2>&1 && err=0 || err=$?
-  if [[ ${err} != 0 ]]; then
-    do_startup_script
-  fi
+  #"${EXEC[@]}" stat "${CHECKPOINT_PATH}" > /dev/null 2>&1 && err=0 || err=$?
+  #if [[ ${err} != 0 ]]; then
+  do_startup_script
+  #fi
 
   sleep "${CHECK_INTERVAL_SECONDS}"
 done
