9f9d1a643a06c1b1098ca90e3dce681090b3c8ed
David Fernandez <morenza@gmail.com>
2021-01-02 22:12:21 +0000

    Initial CF api example

--- go.mod
+++ go.mod
@@ -4,11 +4,12 @@ go 1.13
 
 require (
 	cloud.google.com/go v0.38.0
+	github.com/cloudflare/cloudflare-go v0.13.6
 	github.com/rs/zerolog v1.18.0
 	github.com/smartystreets/goconvey v1.6.4 // indirect
-	github.com/stretchr/testify v1.5.1
+	github.com/stretchr/testify v1.6.1
 	github.com/urfave/cli v1.22.4
-	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e
+	golang.org/x/net v0.0.0-20201202161906-c7110b5ffcbb
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
 	google.golang.org/api v0.21.0
 	google.golang.org/genproto v0.0.0-20190819201941-24fa4b261c55
--- internal/cloudflare/cloudflare_client.go
+++ internal/cloudflare/cloudflare_client.go
@@ -1,1 +1,38 @@
 package cloudflare
+
+import (
+	"fmt"
+	"github.com/cloudflare/cloudflare-go"
+	"log"
+	"os"
+)
+
+func example() {
+	// Construct a new API object
+	api, err := cloudflare.New(os.Getenv("CF_API_KEY"), os.Getenv("CF_API_EMAIL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Fetch user details on the account
+	u, err := api.UserDetails()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Print user details
+	fmt.Println(u)
+
+	// Fetch the zone ID
+	id, err := api.ZoneIDByName("moneycol.net") // Assuming example.com exists in your Cloudflare account already
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Fetch zone details
+	zone, err := api.ZoneDetails(id)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Print zone details
+	fmt.Println(zone)
+}
