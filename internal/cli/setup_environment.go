2c3cc9101113eb349f341c5892c4021e4aa26c57
Przemek Ozgo <linux@ozgo.info>
2021-01-06 11:56:09 +0000

    tidy up

--- internal/cli/setup_environment.go
+++ internal/cli/setup_environment.go
@@ -2,18 +2,11 @@ package cli
 
 import (
 	"fmt"
-	"github.com/iac-io/myiac/internal/gcp"
-	"github.com/iac-io/myiac/internal/util"
 	"github.com/urfave/cli"
-	"os"
 )
 
-func setupEnvironmentCmd(projectFlag *cli.StringFlag, keyPath *cli.StringFlag) cli.Command {
-
-	providerFlag := &cli.StringFlag{
-		Name: "provider",
-		Usage: "Cloud Provider (gcp only for now)",
-	}
+func setupEnvironmentCmd(providerFlag *cli.StringFlag, projectFlag *cli.StringFlag,
+	keyPath *cli.StringFlag) cli.Command {
 
 	return cli.Command{
 		Name:  "setupEnvironment",
@@ -33,7 +26,7 @@ func setupEnvironmentCmd(projectFlag *cli.StringFlag, keyPath *cli.StringFlag) c
 			keyLocation := c.String("keyPath")
 
 			// read these values from config based on project and provider
-			zone := "europe-west1-b"
+			zone := "europe-west2-b"
 			clusterName := "placeholder"
 			setupProvider(providerValue, zone, clusterName, project, keyLocation)
 
@@ -45,12 +38,6 @@ func setupEnvironmentCmd(projectFlag *cli.StringFlag, keyPath *cli.StringFlag) c
 func setupProvider(providerValue string, zone string, clusterName string, project string, keyLocation string) {
 	var provider Provider
 	if providerValue == "gcp" {
-		// Setup ENV Variable with the json credentials
-		err := gcp.SetKeyEnvVar(util.GetGcpKeyFilePath(project))
-		if err != nil {
-			fmt.Printf("GCP: Could not setup Environment Variable. Error: %v", err)
-			os.Exit(1)
-		}
 		gkeCluster := GkeCluster{zone: zone, name: clusterName}
 		provider = NewGcpProvider(project, keyLocation, gkeCluster)
 	} else {
