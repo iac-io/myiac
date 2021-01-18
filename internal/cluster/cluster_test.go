78a56dbcc6208cf73ca9250d43c28819f1e2e89a
pozgo <przemyslaw@ozgo.info>
2021-01-18 15:24:48 +0000

    fixed tests

--- internal/cluster/cluster_test.go
+++ internal/cluster/cluster_test.go
@@ -2,14 +2,16 @@ package cluster
 
 import (
 	"testing"
+
+	"github.com/iac-io/myiac/internal/util"
 )
 
 func TestValidateTFVars(t *testing.T) {
 	got, got1 := ValidateTFVars("/home/app/terraform")
-	if got != "/home/app/terraform" {
-		t.Errorf("ValidateTFVars() got = %v, want %v", got, "/home/app/terraform")
+	if got != util.CurrentExecutableDir()+"/internal/terraform/cluster" {
+		t.Errorf("ValidateTFVars() got = %v, want %v", got, util.CurrentExecutableDir()+"/internal/terraform/cluster")
 	}
-	if got1 != "/home/app/terraform/cluster.tfvars" {
-		t.Errorf("ValidateTFVars() got1 = %v, want %v", got1, "/home/app/terraform/cluster.tfvars")
+	if got1 != util.CurrentExecutableDir()+"/internal/terraform/cluster/cluster.tfvars" {
+		t.Errorf("ValidateTFVars() got1 = %v, want %v", got1, util.CurrentExecutableDir()+"/internal/terraform/cluster")
 	}
 }
