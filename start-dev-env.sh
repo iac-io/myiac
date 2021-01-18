69622ceaab73789414550efc56e0f1508597ce01
pozgo <przemyslaw@ozgo.info>
2021-01-18 16:07:52 +0000

    Added mount path for terraform files.

--- start-dev-env.sh
+++ start-dev-env.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+if [ -z "${2}" ]; then
+  TERRAFORM_PATH='internal/terraform/cluster'
+else
+  TERRAFORM_PATH=${2}
+fi
+
 docker run -ti --rm \
   --name myiac \
   -v ${PWD}/:/workdir \
   -w /workdir \
   -v ${1}:/account.json \
+  -v ${TERRAFORM_PATH}/:/terraform \
   myiac:latest zsh
