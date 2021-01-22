f6c3c84ef72f3560647b75d75498b86d33eb5dc9
David Fernandez <morenza@gmail.com>
2021-01-22 08:31:29 +0000

    Refactor Dockerfile to pick up extra files

--- build-myiac-moneycol-docker.sh
+++ build-myiac-moneycol-docker.sh
@@ -5,5 +5,5 @@ VERSION=app-latest
 
 docker build -t $REGISTRY_WITH_REPO/myiac:$VERSION -f Dockerfiles/Dockerfile . \
 --build-arg CURRENT_HELM_VERSION=2.16.1 \
---build-arg EXTRA_WORKDIR_COPY=/workdir/charts \
+--build-arg EXTRA_WORKDIR_ORIG=/workdir/charts \
 --build-arg EXTRA_WORKDIR_DEST=/home/app/charts
