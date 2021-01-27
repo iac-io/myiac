5c5aea93780bfcb4d6ba99886646af39eb06c552
David Fernandez <morenza@gmail.com>
2021-01-27 08:24:07 +0000

    Fixes #72 Provided SA in key is ignored when running in GCP. Refactor/extraction of GCP auth in its own concern. Missing tests

--- internal/cluster/provider.go
+++ internal/cluster/provider.go
@@ -7,7 +7,6 @@ import (
 	"github.com/iac-io/myiac/internal/commandline"
 	"github.com/iac-io/myiac/internal/gcp"
 	"github.com/iac-io/myiac/internal/preferences"
-	"github.com/iac-io/myiac/internal/util"
 )
 
 const (
@@ -67,32 +66,39 @@ func (pf ProviderFactory) getProvider() Provider {
 }
 
 type gcpProvider struct {
-	projectId  string
-	saKey      *gcp.ServiceAccountKey
-	gkeCluster GkeCluster
-	prefs      preferences.Preferences
+	projectId          string
+	serviceAccountAuth gcp.Auth
+	gkeCluster         GkeCluster
+	prefs              preferences.Preferences
+	commandRunner      commandline.CommandRunner
 }
 
 func NewGcpProvider(projectId string, keyLocation string, gkeCluster GkeCluster) *gcpProvider {
-	key, err := gcp.NewServiceAccountKey(keyLocation)
+	return newGcpProvider(commandline.NewEmpty(), projectId, keyLocation, gkeCluster)
+}
+
+func newGcpProvider(commandRunner commandline.CommandRunner, projectId string, keyLocation string,
+	gkeCluster GkeCluster) *gcpProvider {
+	auth, err := gcp.NewServiceAccountAuth(keyLocation)
 
 	if err != nil {
-		log.Fatal(fmt.Errorf("error obtaining key from location %s: %v", keyLocation, err))
+		log.Fatal(fmt.Errorf("error creating service account auth from key %s: %v", keyLocation, err))
 	}
 
 	return &gcpProvider{
-		projectId:  projectId,
-		saKey:      key,
-		gkeCluster: gkeCluster,
-		prefs:      preferences.DefaultConfig(),
+		projectId:          projectId,
+		serviceAccountAuth: auth,
+		gkeCluster:         gkeCluster,
+		prefs:              preferences.DefaultConfig(),
+		commandRunner:      commandRunner,
 	}
 }
 
-// Setup activate master service account from key
+// Setup activates the master service account from a key file
 func (gp *gcpProvider) Setup() {
-	setupDone := gp.checkSetup()
-	if !setupDone {
-		gp.ActivateServiceAccount()
+	log.Printf("Setting up GCP cloud provider authentication...")
+	if !gp.serviceAccountAuth.IsAuthenticated() {
+		gp.serviceAccountAuth.Authenticate()
 		gp.SetProject()
 		gp.savePreferences()
 	}
@@ -102,53 +108,6 @@ func (gp *gcpProvider) SetProject() {
 	cmdLine := fmt.Sprintf("gcloud config set project %s", gp.projectId)
 	cmd := commandline.NewCommandLine(cmdLine)
 	cmd.Run()
-}
-
-func (gp *gcpProvider) ActivateServiceAccount() {
-	cmdLine := fmt.Sprintf("gcloud auth activate-service-account --key-file %s", gp.saKey.KeyFileLocation)
-	cmd := commandline.NewCommandLine(cmdLine)
-	cmd.Run()
-}
-
-func (gp gcpProvider) checkSetup() bool {
-	providedSaEmail := gp.saKey.Email
-	return isAuthenticated(providedSaEmail)
-}
-
-//TODO:  Extract
-
-func isAuthenticated(saEmail string) bool {
-	authList := listActiveAuth()
-	done := isProvidedSaEmailAuthenticated(saEmail, authList)
-	return done
-}
-
-func listActiveAuth() []map[string]interface{} {
-	cmdLine := fmt.Sprintf("gcloud auth list --format json")
-	cmd := commandline.NewCommandLine(cmdLine)
-	cmdOutput := cmd.Run()
-	authList := util.ParseArray(cmdOutput.Output)
-	return authList
-}
-
-func isProvidedSaEmailAuthenticated(providedSaEmail string, authList []map[string]interface{}) bool {
-	log.Printf("Check if already authenticated with SA: %s", providedSaEmail)
-	for _, accountAuth := range authList {
-		saEmail := accountAuth["account"]
-		status := accountAuth["status"]
-
-		log.Printf("Checking account %s", saEmail)
-
-		// at this point we only allow auth using the provided service account key/email
-		// if running inside GCP we would get multiple ACTIVE SAs: the ones of the service this application
-		// is running on
-		if status == "ACTIVE" && (saEmail == providedSaEmail) {
-			log.Printf("Already authenticated for %s\n", saEmail)
-			return true
-		}
-	}
-	log.Printf("Authentication is needed for SA: %s", providedSaEmail)
-	return false
 }
 
 // ClusterSetup gcloud container clusters get-credentials [cluster-name]
@@ -164,14 +123,12 @@ func (gp gcpProvider) ClusterSetup() {
 	gp.saveGkePreferences(clusterName, zone)
 }
 
-// move to gke.go?
-
 func (gp gcpProvider) savePreferences() {
 	prefs := gp.prefs
 	prefs.Set("provider", gcpProviderName)
-	prefs.Set("keyLocation", gp.saKey.KeyFileLocation)
+	prefs.Set("keyLocation", gp.serviceAccountAuth.Key().KeyFileLocation)
+	prefs.Set("masterSaEmail", gp.serviceAccountAuth.Key().Email)
 	prefs.Set("project", gp.projectId)
-	prefs.Set("masterSaEmail", gp.saKey.Email)
 }
 
 func (gp gcpProvider) saveGkePreferences(clusterName string, zone string) {
--- internal/gcp/auth.go
+++ internal/gcp/auth.go
@@ -1,24 +1,88 @@
 package gcp
 
-import "github.com/iac-io/myiac/internal/commandline"
+import (
+	"fmt"
+	"log"
+
+	"github.com/iac-io/myiac/internal/commandline"
+	"github.com/iac-io/myiac/internal/util"
+)
+
+const (
+	saStatusActive        = "ACTIVE"
+	statusAuthField       = "status"
+	emailAccountAuthField = "account"
+)
+
+type Auth interface {
+	Authenticate()
+	IsAuthenticated() bool
+	Key() *ServiceAccountKey
+}
 
 type ServiceAccountAuth struct {
-	serviceAccountKey ServiceAccountKey
+	ServiceAccountKey *ServiceAccountKey
 	commandRunner     commandline.CommandRunner
 }
 
-func NewServiceAccountAuth(keyLocation string) {
+func NewServiceAccountAuth(keyLocation string) (Auth, error) {
+	saKey, err := NewServiceAccountKey(keyLocation)
 
+	if err != nil {
+		return nil, err
+	}
+
+	return newServiceAccountAuthWithRunner(commandline.NewEmpty(), saKey)
 }
 
-func newServiceAccountAuthWithRunner(commandRunner commandline.CommandRunner, keyLocation string) {
-
+// private constructor that receives commandline (useful for testing)
+func newServiceAccountAuthWithRunner(commandRunner commandline.CommandRunner, accountKey *ServiceAccountKey) (Auth, error) {
+	return &ServiceAccountAuth{ServiceAccountKey: accountKey, commandRunner: commandRunner}, nil
 }
 
-func Authenticate() {
-
+func (saa *ServiceAccountAuth) Authenticate() {
+	keyLocation := saa.ServiceAccountKey.KeyFileLocation
+	cmdLine := fmt.Sprintf("gcloud auth activate-service-account --key-file %s", keyLocation)
+	saa.commandRunner.SetupCmdLine(cmdLine)
+	saa.commandRunner.Run()
 }
 
-func IsAuthenticated() {
+func (saa ServiceAccountAuth) IsAuthenticated() bool {
+	authList := saa.listActiveAuth()
+	done := saa.isServiceAccountEmailAuthenticated(authList)
+	return done
+}
 
+func (saa ServiceAccountAuth) Key() *ServiceAccountKey {
+	return saa.ServiceAccountKey
+}
+
+func (saa ServiceAccountAuth) listActiveAuth() []map[string]interface{} {
+	cmdLine := fmt.Sprintf("gcloud auth list --format json")
+	cmd := commandline.NewCommandLine(cmdLine)
+	cmdOutput := cmd.Run()
+	authList := util.ParseArray(cmdOutput.Output)
+	return authList
+}
+
+func (saa ServiceAccountAuth) isServiceAccountEmailAuthenticated(authList []map[string]interface{}) bool {
+	providedSaEmail := saa.ServiceAccountKey.Email
+
+	log.Printf("Check if already authenticated with SA: %s", providedSaEmail)
+	for _, accountAuth := range authList {
+		saEmail := accountAuth[emailAccountAuthField]
+		status := accountAuth[statusAuthField]
+
+		log.Printf("Checking account %s", saEmail)
+
+		// at this point it's only allowed / considered authentication using the provided service account key.
+		// if running inside GCP there will be multiple ACTIVE SAs: the ones of the service this application
+		// is running on (GKE)
+		if status == saStatusActive && (saEmail == providedSaEmail) {
+			log.Printf("Already authenticated for %s", saEmail)
+			return true
+		}
+	}
+	log.Printf("Authentication is needed for SA: %s", providedSaEmail)
+	return false
 }
