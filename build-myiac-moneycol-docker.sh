b43ccf063b02ab78e6d84168a44e070b0b4847ff
David Fernandez <morenza@gmail.com>
2021-02-16 19:33:07 +0000

    Modified builder script

--- build-myiac-moneycol-docker.sh
+++ build-myiac-moneycol-docker.sh
@@ -4,12 +4,7 @@ export CHARTS_PATH=$1
 REGISTRY_WITH_REPO=eu.gcr.io/moneycol
 VERSION=0.5.0-app
 
-#if [[ -n "$CHARTS_PATH" ]]; then
-#  rsync -rv --exclude=.git "$CHARTS_PATH" .
-#fi
-
 # Before building copy charts from ../charts
-
 docker build -t $REGISTRY_WITH_REPO/myiac:$VERSION -f Dockerfiles/Dockerfile . \
 --build-arg CURRENT_HELM_VERSION=2.16.1 \
 --build-arg EXTRA_WORKDIR_ORIG=/workdir/charts \
